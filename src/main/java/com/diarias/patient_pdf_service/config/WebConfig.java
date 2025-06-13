16c66df40044749ddaedcfb104f3a35b9325ec17
Ferkz <fermon1952@gmail.com>
2025-06-13 17:57:34 -0400

    ajuste security

--- src/main/java/com/diarias/patient_pdf_service/config/WebConfig.java
+++ src/main/java/com/diarias/patient_pdf_service/config/WebConfig.java
@@ -1,19 +1,18 @@
 package com.diarias.patient_pdf_service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://10.0.0.71:3001")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*"); // Todos os headers permitidos
-              WebMvcConfigurer.super.addCorsMappings(registry);
+                .allowedOrigins("http://10.0.0.71:3001") // URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
