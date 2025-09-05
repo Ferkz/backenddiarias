bc0050c7481c7584fa62507a7faa2a020d95bd43
Ferkz <fermon1952@gmail.com>
2025-09-05 09:59:25 -0400

    cors

--- src/main/java/com/diarias/patient_pdf_service/config/SecurityConfig.java
+++ src/main/java/com/diarias/patient_pdf_service/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://10.0.0.71"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
